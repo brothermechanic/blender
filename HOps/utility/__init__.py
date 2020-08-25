820a584a016715feafa4bc893b634e1bb8d87b9e
brothermechanic <brothermechanic@gmail.com>
2020-08-25 11:41:22 +0300

    up

--- HOps/utility/__init__.py
+++
@@ -1,7 +0,0 @@
-import bpy
-
-from bl_ui.space_toolsystem_toolbar import VIEW3D_PT_tools_active as view3d_tools
-
-
-def active_tool():
-    return view3d_tools.tool_active_from_context(bpy.context)
